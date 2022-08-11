ebd9d89d76aea33847971e1fa27f785509d89060
Guan guiwen <694410415@qq.com>
2022-08-11 15:11:20 +0800

    Update Mapping_and_Variant_calling.sh

--- Mapping_and_varicant_calling/Mapping_and_Variant_calling.sh
+++ Mapping_and_varicant_calling/Mapping_and_Variant_calling.sh
@@ -22,27 +22,27 @@ done
 
 for i in $(seq 1 105)
 do
-  echo $i
-        ls S${i}.*1.fq.gz >1
-        ls S${i}.*2.fq.gz >2
-        paste 1 2 >config
-  k=1
-        cat config |while read id
+    echo $i
+    ls S${i}.*1.fq.gz >1
+    ls S${i}.*2.fq.gz >2
+    paste 1 2 >config
+    k=1
+    cat config |while read id
         do
-                arr=($id)
-                fq1=${arr[0]}
-                fq2=${arr[1]}
-                bwa mem -t $threads -M -R "@RG\tID:S$i_$k\tSM:S$i\tLB:WGS\tPL:Illumina" $index $fq1 $fq2 >${fq1}.sam
-                gatk --java-options "-Xmx60G -Djava.io.tmpdir=./"  SortSam \
-				--MAX_RECORDS_IN_RAM 20000000 -SO coordinate -I ${fq1}.sam -O ${fq1}.bam
-                rm ${fq1}.sam
-    k=$((k+1))
-  done
-        bam=S${i}.*.bam
-        samtools merge -@ $threads S${i}.merge.bam $bam
-		gatk --java-options "-Xmx60G -Djava.io.tmpdir=./" MarkDuplicates \
-		--MAX_RECORDS_IN_RAM 20000000 -I S${i}.merge.bam -O S${i}.merge.bam.marked.bam \
-        -M S${i}.merge.bam.marked.bam.metrics
+            arr=($id)
+            fq1=${arr[0]}
+            fq2=${arr[1]}
+            bwa mem -t $threads -M -R "@RG\tID:S$i_$k\tSM:S$i\tLB:WGS\tPL:Illumina" $index $fq1 $fq2 >${fq1}.sam
+            gatk --java-options "-Xmx60G -Djava.io.tmpdir=./"  SortSam \
+		    --MAX_RECORDS_IN_RAM 20000000 -SO coordinate -I ${fq1}.sam -O ${fq1}.bam
+            rm ${fq1}.sam
+            k=$((k+1))
+        done
+    bam=S${i}.*.bam
+    samtools merge -@ $threads S${i}.merge.bam $bam
+    gatk --java-options "-Xmx60G -Djava.io.tmpdir=./" MarkDuplicates \
+    --MAX_RECORDS_IN_RAM 20000000 -I S${i}.merge.bam -O S${i}.merge.bam.marked.bam \
+    -M S${i}.merge.bam.marked.bam.metrics
     samtools index -@ $threads S${i}.merge.bam.marked.bam
 done
 
